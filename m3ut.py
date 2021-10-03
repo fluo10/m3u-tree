ace09592f787c56ff29585edde039482df6d7d1c
fluo10 <fluo10.dev@fireturtle.net>
2021-10-03 12:36:04 +0900

    Enable to get tracks from m3u

--- m3ut.py
+++ m3ut.py
@@ -15,18 +15,33 @@ class Playlist:
     def __init__(self, path, name):
         self.path = path
         self.name = name
-        self.tracks = []
+        self.tracks = set()
     def add_track(self, track):
-        self.tracks.append(track)
-    def import_m3u(self, file):
-        if args.verbose :
-            print('import file')
+        self.tracks.add(track)
+    def read_tracks(self):
+        dir = os.path.dirname(self.path)
+        f = open(self.path, 'r', errors='replace')
+        while True:
+            line = f.readline().rstrip()
+            if line == '':
+                break
+            if os.path.isabs(line):
+                self.tracks.add(line)
+            else:
+                self.tracks.add(os.path.abspath(os.path.join(dir, line)))   
     def compare(self, another):
         if args.verbose :
             print('compare')
     def is_branch(self):
         return os.path.basename(self.path).removesuffix('.m3u') != self.name
-#    def read_content(self):
+    def show_summary(self, prefix):
+        text = prefix + self.path
+        count = len(self.tracks)
+        if count > 0:
+            count_str = '(' + str(count) + ')'
+        else :
+            count_str = '' 
+        print(prefix + self.path + count_str)
 
 #    def write_content(self):
 
@@ -35,19 +50,58 @@ class PlaylistTree:
         self.name = name
         self.root_playlist = None
         self.branch_playlists = []
-        self.tracks = []
+        self.branch_tracks = set()
 #    def read_contents(self):
+    def read_tracks(self):
+        if not self.root_playlist is None: 
+            self.root_playlist.read_tracks()
+        for playlist in self.branch_playlists:
+            playlist.read_tracks()
+            self.branch_tracks = self.branch_tracks |  playlist.tracks
+
+
     def show(self):
         print(self.name)
+        if len(self.branch_tracks) > 0 :
+            count_str = '(' + str(len(self.branch_tracks)) + ')'
+        else :
+            count_str = ""
+        print ('  root: ')
         if self.root_playlist is None:
-            print('  root: None')
+            print('    None')
         else:
-            print('  root: ' + self.root_playlist.path)
+            print('    ' + self.root_playlist.path)
         
-        print('  branches:')
+        print('  branches' + count_str + ':')
         for branch in self.branch_playlists:
-            print ('    ' + branch.path)
+            branch.show_summary('    ')
         print('')
+
+    def diff(self):
+        print(self.name)
+        if self.root_playlist is None:
+            root_tracks = set()
+        else: 
+            root_tracks = self.root_playlist.tracks
+        
+
+        # Tracks to keep
+        print('  Keep:')
+        for track in self.branch_tracks & root_tracks:
+            print('    ' + track)
+
+
+        # Tracks to add
+        print('  Add:')
+        for track in self.branch_tracks - root_tracks:
+            print('    ' + track)
+        
+        # Tracks to delete
+        print('  Delete:')
+        for track in root_tracks - self.branch_tracks:
+            print('    ' + track)
+
+        
     
 class PlaylistTrees:
     def __init__(self, args):
@@ -55,9 +109,10 @@ class PlaylistTrees:
         self.prefix = args.tree_prefix
         self.suffix = args.tree_suffix
         self.tree_dict = None
-        self.reflesh()
+        self.reflesh_list()
+        self.read_tracks()
 
-    def reflesh(self):
+    def reflesh_list(self):
         if not os.path.isdir(self.root):
             print('Error: ' + self.root + ' is not directory')
 
@@ -65,7 +120,11 @@ class PlaylistTrees:
         for root, dirs, files in os.walk(self.root):
             for name in files:
                 self.parse_tree_dict(os.path.join(root, name))
-    
+
+    def read_tracks(self):
+        for tree in self.tree_dict.values():
+            tree.read_tracks()
+
     def parse_tree_dict(self, path):
         dir = os.path.dirname(path)
         file = os.path.basename(path)
@@ -85,33 +144,18 @@ class PlaylistTrees:
             tree.branch_playlists.append(playlist)
         else:
             tree.root_playlist = playlist
-    
             
     def show(self):
         for tree in self.tree_dict.values():
             tree.show()
+    def diff(self):
+        for tree in self.tree_dict.values():
+            tree.diff()
 
+    def compile(self):
+        print()
 
 playlist_dict = {}
-
-def show_m3u(targetDirPath):
-    result = []
-    if not os.path.isdir(targetDirPath):
-        print('Error: ' +targetDirPath + ' is not directory')
-
-    for root, dirs, files in os.walk(targetDirPath):
-        for name in files:
-            # m3u_match =  m3u_pattern.fullmatch(name)
-            # if m3u_match:
-            if name.endswith('.m3u'):
-                result.append(os.path.abspath(os.path.join(root, name)))
-                #if args.verbose :
-                #    print(name)
-    return result
-def show_m3u_tree(rootDir):
-    result = {}
-    playlists = show_m3u(rootDir)
-        
 
 
 def m3ut_compile(args):
@@ -121,6 +165,9 @@ def m3ut_compile(args):
 def m3ut_show(args):
     print('m3u-tools show')
     print(PlaylistTrees(args).show())
+
+def m3ut_diff(args):
+    PlaylistTrees(args).diff()
 
 def m3ut_check(args):
     print('m3u-tools check')
@@ -149,6 +196,9 @@ parser_fix.set_defaults(func=m3ut_fix)
 
 parser_show = subparsers.add_parser('show', help='show help')
 parser_show.set_defaults(func=m3ut_show)
+
+parser_show = subparsers.add_parser('diff', help='diff help')
+parser_show.set_defaults(func=m3ut_diff)
 
 parser.add_argument('-d', '--directory', default='~/Music', help='Root directory to save merged m3u file.')
 parser.add_argument('-n', '--dry-run', action='store_true')
